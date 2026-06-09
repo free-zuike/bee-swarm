4d86e71f4b775b2ce15c98fa6a269103e7d61ead
Developer <dev@example.com>
2026-06-09 03:01:47 +0000

    fix: 删除重复的迁移文件0004_add_recurring_fields.sql

--- migrations/0004_add_recurring_fields.sql
+++
@@ -1,4 +0,0 @@
--- 添加定时任务的重复类型和相关字段
-ALTER TABLE scheduled_pushes ADD COLUMN recurring_type TEXT;
-ALTER TABLE scheduled_pushes ADD COLUMN selected_week_days TEXT;
-ALTER TABLE scheduled_pushes ADD COLUMN selected_month_days TEXT;
